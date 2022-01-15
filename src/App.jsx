48ace5d569a4c78c35da5a66ad8c33170e96f453
warugaki <warugaki.web.developer@gmail.com>
2022-01-15 10:29:41 +0900

    都市名を追加（ベダ書きで）

--- src/App.jsx
+++ src/App.jsx
@@ -33,6 +33,7 @@ const App = () => {
       <h1>Minimal Weather App with React</h1>
       <div className="weather">
         <p>{weatherData.current_weather.time}</p>
+        <p>OSAKA</p>
         <p>
           <span>{weatherData.current_weather.temperature}</span>
           <span>{weatherData.hourly_units.temperature_2m}</span>
