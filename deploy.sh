2952d537d5adb458644a0e459592c25728bad4ac
warugaki <warugaki.web.developer@gmail.com>
2022-01-30 11:02:09 +0900

    GitHub Pagesにdeployできるように

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env sh
+
+# エラー時は停止
+set -e
+
+# ビルド
+npm run build
+
+# ビルド出力ディレクトリに移動
+cd dist
+
+# カスタムドメインにデプロイする場合
+# echo 'www.example.com' > CNAME
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# https://<USERNAME>.github.io にデプロイする場合
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+git push -f git@github.com:warugaki-web-developer/minimal-weather-app-with-react.git main:gh-pages
+
+cd -
